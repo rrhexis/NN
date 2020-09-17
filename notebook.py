ff601679896f2cff522f2175fa70900e2d5202ce
Anita <71455746+rrhexis@users.noreply.github.com>
2020-09-17 19:45:36 +0300

    Add files via upload

---
+++ notebook.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+
+import matplotlib.pyplot as plt
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train.shape
+
+plt.title(y_train[10])
+plt.imshow(x_train[10], cmap=plt.get_cmap('gray_r'))
+plt.show()
+
+def display_digit(num):
+    label = y_train[num]
+    image = x_train[num]
+    plt.title('Example: {}  Label: {}'.format(num, label))
+    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+    plt.show()
+
+display_digit(2908)
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(100, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.0),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+              loss=tf.keras.losses.sparse_categorical_crossentropy,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+loss, acc = model.evaluate(x_test, y_test)
+print("Loss = {}, accuracy = {}".format(loss, acc))
+loss, acc = model.evaluate(x_train, y_train)
+print("Loss = {}, accuracy = {}".format(loss, acc))
+
+predictions = model.predict(x_test[0:1,:,:])
+print(predictions)
+print(y_test[0])
