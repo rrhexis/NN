26760d4426e0dbb05a384f2099f386b50a25f529
rrhexis <anita.rogovik@mail.ru>
2020-09-23 22:46:25 +0300

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -10,18 +10,18 @@ x_train.shape
 
 plt.title(y_train[10])
 plt.imshow(x_train[10], cmap=plt.get_cmap('gray_r'))
-#plt.show()
+plt.show()
 
 def display_digit(num):
-    label = y_train[num]
-    image = x_train[num]
-    plt.title('Example: {}  Label: {}'.format(num, label))
-    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-    plt.show()
+  label = y_train[num]
+  image = x_train[num]
+  plt.title('Example: {}  Label: {}'.format(num, label))
+  plt.imshow(image, cmap=plt.get_cmap('gray_r'))
+  plt.show()
 
-#display_digit(2908)
+  display_digit(2908)
 
-model = tf.keras.models.Sequential([
+  model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(100, activation=tf.nn.relu),
   tf.keras.layers.Dropout(0.0),
@@ -36,10 +36,10 @@ model.fit(x_train, y_train, epochs=3)
 
 loss, acc = model.evaluate(x_test, y_test)
 print("Loss = {}, accuracy = {}".format(loss, acc))
+
 loss, acc = model.evaluate(x_train, y_train)
 print("Loss = {}, accuracy = {}".format(loss, acc))
 
 predictions = model.predict(x_test[0:1,:,:])
 print(predictions)
 print(y_test[0])
-print("Hello")
