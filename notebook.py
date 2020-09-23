5f4c70205246365913685b4634e33b405abeb299
rrhexis <anita.rogovik@mail.ru>
2020-09-23 23:53:22 +0300

    Update notebook.py
    точность ~98%

--- notebook.py
+++ notebook.py
@@ -8,23 +8,11 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 x_train.shape
 
-plt.title(y_train[10])
-plt.imshow(x_train[10], cmap=plt.get_cmap('gray_r'))
-plt.show()
-
-def display_digit(num):
-  label = y_train[num]
-  image = x_train[num]
-  plt.title('Example: {}  Label: {}'.format(num, label))
-  plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-  plt.show()
-
-display_digit(2908)
-
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(100, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.0),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(80, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.1),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
@@ -32,7 +20,7 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=10)
 
 loss, acc = model.evaluate(x_test, y_test)
 print("Loss = {}, accuracy = {}".format(loss, acc))
