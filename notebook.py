118695afdc918b573a13c2476612210d8f4b7fc9
rrhexis <anita.rogovik@mail.ru>
2020-10-01 15:37:24 +0300

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -10,27 +10,6 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 x_train.shape
 
-def display_digit(num, x, y, vector = None):
-  label = y[num]
-  image = x[num]
-  if vector is None:
-    plt.title('Example: {}  Label: {}'.format(num, label))
-    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-  else:
-    plt.figure(figsize=(6,3))
-    plt.subplot(1,2,1)
-    plt.title('Real label: {}'.format(label))
-    plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-    plt.subplot(1,2,2)
-    thisplot = plt.bar(range(10), vector, color="#777777")
-    plt.ylim([0, 1]) 
-    plt.xticks([])
-    plt.yticks([])
-    predicted_label = np.argmax(vector)
-    thisplot[predicted_label].set_color('red')
-    plt.title('Predicted label: {}'.format(predicted_label))
-  plt.show()
-
 #overfit model
 model_overfit = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -43,7 +22,7 @@ model_overfit = tf.keras.models.Sequential([
 model_overfit.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
-history_overfit = model_overfit.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test))
+history_overfit = model_overfit.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test))
 
 loss_overfit, acc_overfit = model_overfit.evaluate(x_test, y_test)
 print("Loss = {}, accuracy = {}".format(loss_overfit, acc_overfit))
@@ -63,7 +42,7 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
-history = model.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test))
+history = model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test))
 
 loss, acc = model.evaluate(x_test, y_test)
 print("Loss = {}, accuracy = {}".format(loss, acc))
@@ -86,8 +65,6 @@ plt.legend()
 plt.xlim([0,max(history.epoch)])
 plt.show()
 
-for i in range(10):
-    display_digit(i,x_test,y_test,model(x_test[i:i+1,:,:])[0])
 
 print("loss(overfit) / loss(standart) = ", loss_overfit / loss)
 
