a47b440d4cb0e3423d41f0154af64997cae7adae
rrhexis <anita.rogovik@mail.ru>
2020-10-09 00:36:47 +0300

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -6,21 +6,6 @@ import matplotlib.pyplot as plt
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-x_train.shape
-
-plt.title(y_train[10])
-plt.imshow(x_train[10], cmap=plt.get_cmap('gray_r'))
-plt.show()
-
-def display_digit(num):
-  label = y_train[num]
-  image = x_train[num]
-  plt.title('Example: {}  Label: {}'.format(num, label))
-  plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-  plt.show()
-
-display_digit(2908)
-
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(100, activation=tf.nn.relu),
@@ -31,8 +16,7 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
-
-model.fit(x_train, y_train, epochs=3)
+history = model.fit(x_train, y_train, epochs=3, validation_data = (x_test, y_test))
 
 loss, acc = model.evaluate(x_test, y_test)
 print("Loss = {}, accuracy = {}".format(loss, acc))
@@ -40,6 +24,12 @@ print("Loss = {}, accuracy = {}".format(loss, acc))
 loss, acc = model.evaluate(x_train, y_train)
 print("Loss = {}, accuracy = {}".format(loss, acc))
 
-predictions = model.predict(x_test[0:1,:,:])
-print(predictions)
-print(y_test[0])
+plt.figure(figsize=(10,7))
+plt.xlabel('Epochs')
+plt.ylabel("Sparse_categorical_crossentropy")
+val = plt.plot(history.epoch, history.history['accuracy'],
+                   '--', label='Val', color = 'blue')
+
+plt.legend()
+plt.xlim([0,max(history.epoch)])
+plt.show()
