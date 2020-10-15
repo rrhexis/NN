77a9b7c319cdeb0fd154d8be1425d1ff05134cc2
rrhexis <anita.rogovik@mail.ru>
2020-10-15 16:59:53 +0300

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,45 +1,81 @@
-import tensorflow as tf
-mnist = tf.keras.datasets.mnist
-
+from PIL import Image, ImageFilter
+import numpy as np
+import requests
+from io import BytesIO
 import matplotlib.pyplot as plt
+from scipy import signal
+import tensorflow as tf
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Conv2D, \
+                                    Dense, \
+                                    MaxPool2D,\
+                                    Dropout, \
+                                    Flatten, \
+                                    BatchNormalization
+from tensorflow.keras.datasets import mnist
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import itertools
+
+
+mnist = tf.keras.datasets.mnist
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-x_train.shape
+x_train = np.expand_dims(x_train, axis=3)
+x_test = np.expand_dims(x_test, axis=3)
+input_shape = x_train[0].shape
 
-plt.title(y_train[10])
-plt.imshow(x_train[10], cmap=plt.get_cmap('gray_r'))
-plt.show()
+model = Sequential([
+  Conv2D(32,kernel_size=(3,3), input_shape=input_shape, activation ='relu', padding='same'),
+  MaxPool2D(pool_size=(2, 2)),
 
-def display_digit(num):
-  label = y_train[num]
-  image = x_train[num]
-  plt.title('Example: {}  Label: {}'.format(num, label))
-  plt.imshow(image, cmap=plt.get_cmap('gray_r'))
-  plt.show()
+	Conv2D(32,kernel_size=(3,3), activation ='relu', padding='same'),
+	MaxPool2D(pool_size=(2, 2)),
 
-display_digit(2908)
+	Conv2D(32,kernel_size=(3,3), activation ='relu', padding = 'same'),
+  MaxPool2D(pool_size=(2, 2)),
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(100, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.0),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+	Conv2D(64,kernel_size=(3,3), activation ='relu', padding='same'),
+  MaxPool2D(pool_size=(2, 2)),
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+  Conv2D(64,kernel_size=(3,3), activation ='relu', padding='same'),
+
+	Flatten(),
+  Dense(128, activation=tf.nn.leaky_relu),
+  Dropout(0.25),
+  Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer="adam",
               loss=tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
+model.summary()
 
-model.fit(x_train, y_train, epochs=3)
+history = model.fit(x_train,
+                    y_train,
+                    epochs=1,
+                    validation_data=(x_test, y_test))
 
-loss, acc = model.evaluate(x_test, y_test)
-print("Loss = {}, accuracy = {}".format(loss, acc))
+
 
 loss, acc = model.evaluate(x_train, y_train)
 print("Loss = {}, accuracy = {}".format(loss, acc))
+loss, acc = model.evaluate(x_test, y_test)
+print("Loss = {}, accuracy = {}".format(loss, acc))
 
-predictions = model.predict(x_test[0:1,:,:])
-print(predictions)
-print(y_test[0])
+layer_outputs = [layer.output for layer in model.layers[:-1]]
+activation_model = tf.keras.Model(inputs = model.input, outputs = layer_outputs)
+
+test_im = x_train[2979] # Random image
+activations = activation_model.predict(test_im.reshape(1,28,28,1))
+layer_activation = activations[0]
+
+fig, ax = plt.subplots()
+
+ax.imshow(layer_outputs)
+
+fig.set_figwidth(6)    #  ширина и
+fig.set_figheight(6)    #  высота "Figure"
+
+plt.show()
